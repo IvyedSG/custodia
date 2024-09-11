eebefbfdb5b6fa1bc9fe37a51c16c7ada3798c02
IvyedSG <deyvioscarzanches@gmail.com>
2024-09-11 18:56:26 -0500

    update

--- app/pages/lockers/page.jsx
+++ app/pages/lockers/page.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { motion } from 'framer-motion';
-import LockerButton from './LockerButton';
+import LockerButton from './components/LockerButton';
 
 export default function LockerGrid({ lockers, onLockerClick }) {
   return (
